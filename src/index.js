bfc9506ec4b38add3bd2342dd8beed6d3ad8deee
Majid Ali Khan <majidalykhan@gmail.com>
2022-08-02 18:51:51 +0500

    JSX Rules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// JSX rules
+// return single element
+// div / section / article or Fragment
+// use camelCase for html attribute
+// close every element 
+// formatting
+
 function Hello() {
-  return <h3>This is first component</h3>;
+  return (
+    <div>
+      <h1>This is first component</h1>
+    </div>
+  );
 }
 
 ReactDOM.render(<Hello />, document.getElementById("root"));
