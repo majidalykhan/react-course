cd327bee271bdf4afccfb9086369d7f8ecdfbe25
Majid Ali Khan <majidalykhan@gmail.com>
2022-08-03 17:26:01 +0500

    Proper list

--- src/index.js
+++ src/index.js
@@ -14,27 +14,32 @@ const books = [
     author: "Jenny Han",
     img: "https://images-na.ssl-images-amazon.com/images/I/51ky4TIb1bL._AC_SX184_.jpg",
   },
+  {
+    title: "The Lord Of The Rings",
+    author: "J.R.R. Tolkien",
+    img:'https://images-na.ssl-images-amazon.com/images/I/51kfFS5-fnL._AC_SX184_.jpg',
+  },
+  
 ];
 
-//Simple list example
-const movies = ["Iron Man", "Batman", "Harry Potter"];
-
-const newMovies = movies.map((movie) => {
-  return <h1>{movie}</h1>;
-});
-
 function BookList() {
-  return <section className="booklist">{newMovies}</section>;
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book book={book} />;
+      })}
+    </section>
+  );
 }
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props.book;
   return (
     <article className="book">
       <img src={img}></img>
       <h2>{title}</h2>
       <h3>{author}</h3>
-      {children}
     </article>
   );
 };
