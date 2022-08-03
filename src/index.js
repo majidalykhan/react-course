eb3cab92f7c0304ab226a9ecec44aa568e5c361e
Majid Ali Khan <majidalykhan@gmail.com>
2022-08-03 15:42:19 +0500

    Props

--- src/index.js
+++ src/index.js
@@ -3,22 +3,41 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
+const firstBook = {
+  title: "Reminders of Him: A Novel",
+  author: "Colleen Hoover",
+  img: "https://images-na.ssl-images-amazon.com/images/I/41n9-p6-PpL._AC_SX184_.jpg",
+};
+
+const secondBook = {
+  title: "It's Not Summer Without You",
+  author: "Jenny Han",
+  img: "https://images-na.ssl-images-amazon.com/images/I/51ky4TIb1bL._AC_SX184_.jpg",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        image={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        image={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
-  const title = "Reminders of Him: A Novel";
-  const author = "Colleen Hoover";
+const Book = (props) => {
   return (
     <article className="book">
-      <img src="https://images-na.ssl-images-amazon.com/images/I/41n9-p6-PpL._AC_SX184_.jpg"></img>
-      <h2>{title}</h2>
-      <h3>{author.toUpperCase()}</h3>
+      <img src={props.image}></img>
+      <h2>{props.title}</h2>
+      <h3>{props.author}</h3>
     </article>
   );
 };
