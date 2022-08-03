8ee78ffd17e65602d4aaa54e88c6822676220649
Majid Ali Khan <majidalykhan@gmail.com>
2022-08-03 16:20:57 +0500

    Simple list example using map function

--- src/index.js
+++ src/index.js
@@ -3,38 +3,28 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-const firstBook = {
-  title: "Reminders of Him: A Novel",
-  author: "Colleen Hoover",
-  img: "https://images-na.ssl-images-amazon.com/images/I/41n9-p6-PpL._AC_SX184_.jpg",
-};
+const books = [
+  {
+    title: "Reminders of Him: A Novel",
+    author: "Colleen Hoover",
+    img: "https://images-na.ssl-images-amazon.com/images/I/41n9-p6-PpL._AC_SX184_.jpg",
+  },
+  {
+    title: "It's Not Summer Without You",
+    author: "Jenny Han",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ky4TIb1bL._AC_SX184_.jpg",
+  },
+];
 
-const secondBook = {
-  title: "It's Not Summer Without You",
-  author: "Jenny Han",
-  img: "https://images-na.ssl-images-amazon.com/images/I/51ky4TIb1bL._AC_SX184_.jpg",
-};
+//Simple list example
+const movies = ["Iron Man", "Batman", "Harry Potter"];
+
+const newMovies = movies.map((movie) => {
+  return <h1>{movie}</h1>;
+});
 
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        {/*Content in the opening and closing tag of component is Props chidlren */}
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-        asperiores!
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+  return <section className="booklist">{newMovies}</section>;
 }
 
 const Book = (props) => {
