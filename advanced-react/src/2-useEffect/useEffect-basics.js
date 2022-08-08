c86caa7a7626e0b04da65475f8e5100d1266aeba
Majid Ali Khan <majidalykhan@gmail.com>
2022-08-08 17:34:47 +0500

    useEffect - Dependency List

--- advanced-react/src/2-useEffect/useEffect-basics.js
+++ advanced-react/src/2-useEffect/useEffect-basics.js
@@ -8,12 +8,25 @@ import React, { useState, useEffect } from "react";
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
 
-  useEffect(() => {
-    console.log("Call useEffect 1");
-    if (value > 0) {
-      document.title = `New Message(${value})`;
-    }
-  });
+  // By default useEffect will run on every re-render
+  useEffect(
+    () => {
+      console.log("Call useEffect 1");
+      if (value > 0) {
+        document.title = `New Message(${value})`;
+      }
+    },
+    [value] /* Second parameter - If empty, useEffect will run on initial render
+                            if any value changes in list, useEffect will run each time */
+  );
+
+  //Can have unlimited useEffects
+  useEffect(
+    () => {
+      console.log("Call useEffect 2");
+    },
+    [] /* Will only run on initial render*/
+  );
 
   console.log("Render Component");
 
