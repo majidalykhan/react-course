5fe6df7fb9221ead2483b922393e47a77b1dbdae
Majid Ali Khan <majidalykhan@gmail.com>
2022-08-09 18:09:38 +0500

    Forms - Controlled inputs

--- advanced-react/src/4-forms/1-controlled-inputs.js
+++ advanced-react/src/4-forms/1-controlled-inputs.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Hello World");
+    console.log(firstName, email);
   };
 
   return (
@@ -12,11 +15,23 @@ const ControlledInputs = () => {
         <form className="form" onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="firstName">Name : </label>
-            <input type="text" id="firstName" name="firstName" />
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="email">Email : </label>
-            <input type="text" id="email" name="email" />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <button type="submit" className="btn">
             Add Person
