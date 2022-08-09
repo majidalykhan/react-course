27be555f40e63c325119d3faa846e76c84a8c20d
Majid Ali Khan <majidalykhan@gmail.com>
2022-08-09 16:18:55 +0500

    Ternary Operator

--- advanced-react/src/3-conditional-rendering/2-short-circuit.js
+++ advanced-react/src/3-conditional-rendering/2-short-circuit.js
@@ -2,12 +2,20 @@ import React, { useState, useEffect } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("");
-  const firstValue = text || "Hello World"; //first value shows Hello World because || operator check if value is empty or falsy then show the other one
-  const secondValue = text && "Hello World"; //second value shows '' empty because && operator check the value and show the first one available
+  const [isError, setIsError] = useState(false);
   return (
     <div>
-      <h2>Value 1: {firstValue}</h2>
-      <h2>Value 2: {secondValue}</h2>
+      <h2>{text || "John"}</h2>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toggle Error
+      </button>
+      {isError ? (
+        <p>There is an error...</p>
+      ) : (
+        <div>
+          <p>There is no error</p>
+        </div>
+      )}
     </div>
   );
 };
